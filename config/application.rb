90ff244570870716a11e5c9b262099c97ef22a6f
deebot10 <dee1jhill@gmail.com>
2021-10-18 16:44:19 -0600

    edit code

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
 
-# config.middleware.use Rack::MethodOverride
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
