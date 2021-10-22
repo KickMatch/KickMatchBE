721782b4719407c11dc4bc973b4fd5471fba5bd5
deebot10 <dee1jhill@gmail.com>
2021-10-22 14:43:39 -0600

    Final touches to match

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -19,11 +19,6 @@ module Types
       Talent.find(id)
     end
 
-    def talent_match(id:)  
-      Match.all.where('talent_id = ?', id)    
-    end
-
-    #SportClubs 
     field :all_clubs, [Types::SportClubType], null: false 
 
     def all_clubs
@@ -38,28 +33,12 @@ module Types
       SportClub.find(id)
     end
 
-    # Match 
-    # field :match, [Types::MatchType], null: false 
-      # field :club
-      # field :talent, [Types::TalentType], null: false do
-      #   argument :id, ID, required: true
-      # end
+    field :match_talent, [Types::MatchType], null: false do 
+      argument :id, ID, required: true
+    end
 
-    # def match_talent  
-    #   Match.all #.where('talent_id = ?', id)    
-    # end
-
-    #  field :match_talent, Types::MatchType, null: false do
-    #    argument :id, ID, required: true
-    #  end
-
-    # def match_talent(id:)
-    #   t = Talent.find(id) 
-    #   t.matches.all
-    #   # Match.all.where('talent_id = ?', id)    
-    #   # require 'pry'; binding.pry
-    # end 
-
-    # end
+    def match_talent(id:)
+      Match.all.where('talent_id = ?', id)
+    end
   end
 end
