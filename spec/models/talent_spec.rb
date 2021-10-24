628f76451347781f69022b2adcc42dd90c0273db
JoannaCoPo <jococopo@gmail.com>
2021-10-24 00:18:40 -0600

    Test model validations

--- spec/models/talent_spec.rb
+++ spec/models/talent_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Talent do
     it {should have_many(:matches) }
     it {should have_many(:sport_clubs).through(:matches) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:age) }
+    it { should validate_presence_of(:height) }
+    it { should validate_presence_of(:weight) }
+    it { should validate_presence_of(:primary_position) }
+    it { should validate_presence_of(:secondary_position) }
+    it { should validate_presence_of(:video_url) }
+  end
 end
