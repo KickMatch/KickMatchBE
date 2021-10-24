f055473a33fbf7004a528047dfed6d7ef4fb951e
JoannaCoPo <jococopo@gmail.com>
2021-10-24 00:13:25 -0600

    Remove bang in talent creation to avoid active record error while testing

--- app/graphql/mutations/create_talent.rb
+++ app/graphql/mutations/create_talent.rb
@@ -11,7 +11,7 @@ class Mutations::CreateTalent < Mutations::BaseMutation
 	field :errors, [String], null: false
 
   def resolve(name:, age:, height:, weight:, primary_position:, secondary_position:, video_url:)
-    talent = Talent.create!(
+    talent = Talent.create(
       name: name,
       age: age,
       height: height,
