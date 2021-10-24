92d9b59f58cbf7caa9a23e2f643830ea43b760f1
JoannaCoPo <jococopo@gmail.com>
2021-10-24 00:12:32 -0600

    Add presence validations for talent model

--- app/models/talent.rb
+++ app/models/talent.rb
@@ -1,4 +1,11 @@
 class Talent < ApplicationRecord
   has_many :matches
-  has_many :sport_clubs, through: :matches  
+  has_many :sport_clubs, through: :matches
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :height, presence: true
+  validates :weight, presence: true
+  validates :primary_position, presence: true
+  validates :secondary_position, presence: true
+  validates :video_url, presence: true
 end
