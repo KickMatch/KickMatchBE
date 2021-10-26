0b6f1bb3751590619c193d8fe0201eb9620f45c9
deebot10 <dee1jhill@gmail.com>
2021-10-26 14:41:17 -0600

    Edit test data in get_one_club_spec

--- spec/graphql/types/query_type/get_one_club_spec.rb
+++ spec/graphql/types/query_type/get_one_club_spec.rb
@@ -10,7 +10,8 @@ RSpec.describe Types::QueryType, type: :request do
       types = GraphQL::Define::TypeDefiner.instance
 
       it 'returns club details' do
-          @team1 = create(:sport_club)
+          @team1 = SportClub.create(name: 'Denver SoccerClub', location: 'Denver CO', league: 'G leagure', zipcode: 80207, email: 'denfutbol@gmail.com', phone: '720-555-4114', tournaments: 'Colorado Championchip', coach_resume: 'Fred Hampton is a motivated Illinois Native interested in using his pro experience to guide youth to their full potential.', currently_looking: 'Goalie,  Mid Foward')
+
 
           post '/graphql', params: { query: club_query }
 
@@ -24,7 +25,8 @@ RSpec.describe Types::QueryType, type: :request do
     end
 
     it 'can query for a single instance of sport club' do
-      @team1 = create(:sport_club)
+        @team1 = SportClub.create(name: 'Denver SoccerClub', location: 'Denver CO', league: 'G leagure', zipcode: 80207, email: 'denfutbol@gmail.com', phone: '720-555-4114', tournaments: 'Colorado Championchip', coach_resume: 'Fred Hampton is a motivated Illinois Native interested in using his pro experience to guide youth to their full potential.', currently_looking: 'Goalie,  Mid Foward')
+
       result = KickMatchBESchema.execute(club_query).as_json
       query_result = result["data"]["club"]
       expect(query_result["id"]).to eq(@team1.id.to_s)
